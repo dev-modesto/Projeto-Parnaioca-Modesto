1df3ab0c6eaf1d6d62b42a9bd3c05b270392a92b
Gabriel.Modesto <gabriel_26@outlook.com.br>
2024-06-20 15:00:43 -0300

    fix: [não completo] ajustes caminhos

--- include/navbar-lateral/navbar-lateral.php
+++ include/navbar-lateral/navbar-lateral.php
@@ -1,5 +1,6 @@
 <?php
-    include 'head.php';
+    include '../../include/head.php';
+    
 ?>
 
 <header class="header">
@@ -12,7 +13,7 @@
 
     <ul class="navbar-itens">
         <li class="cor-3 "><a href="navbar-lateral.php" class="font-1-m"><span class="material-symbols-rounded">dashboard</span>Dashboard</a></li>
-        <li class="cor-3"><a href="#" class="font-1-m"><span class="material-symbols-rounded">style</span>Reservas</a></li>
+        <li class="cor-3"><a href="app/" class="font-1-m"><span class="material-symbols-rounded">style</span>Reservas</a></li>
         <li class="cor-3"><a href="#" class="font-1-m"><span class="material-symbols-rounded">hotel</span>Acomodações</a></li>
         <li class="cor-3"><a href="#" class="font-1-m"><span class="material-symbols-rounded">group</span>Clientes</a></li>
         <li class="cor-3"><a href="#" class="font-1-m"><span class="material-symbols-rounded">directions_car</span>Estacionamento</a></li>
