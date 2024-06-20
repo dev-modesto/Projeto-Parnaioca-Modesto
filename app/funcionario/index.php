dd6ffc856c0816bab19cab7cb62817b775d50a1e
Gabriel.Modesto <gabriel_26@outlook.com.br>
2024-06-20 15:02:39 -0300

    style: ajustes view funcionarios

--- app/funcionario/index.php
+++ app/funcionario/index.php
@@ -1,44 +1,55 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php
+    include '../../include/navbar-lateral/navbar-lateral.php';
+?>
+<div class="conteudo">
+
 
     <!-- formulario envio cargo -->
-    <form action="include/gFuncionario.php" method="post">
-        <label for="nome">Nome</label><br>
-        <input type="text" name="nome" required>
-        <br>
-    
-        <label for="cpf">CPF</label><br>
-        <input type="text" name="cpf" required>
-        <br>
+    <form class="form-container" action="include/gFuncionario.php" method="post">
+        <h1>Cadastrar Funcionario</h1>
+        <span class="separador"></span>
+        <div class="mb-3">
+            <label class="font-1-s" for="nome">Nome completo</label>
+            <input class="form-control" type="text" name="nome" required>
+        </div>
 
-        <label for="telefone">Telefone</label><br>
-        <input type="fone" name="telefone" required>
-        <br>
-    
-        <label for="id_cargo">Cargo</label><br>
-        <select name="id_cargo" required>
-            <?php
-                include '../../config/conexao.php';
-                $query = "SELECT id_cargo, nome_cargo FROM tbl_cargo";
-                $result = mysqli_query($con, $query);
-    
-                while ($row = mysqli_fetch_assoc($result)) {
-                    echo "<option value='" . $row['id_cargo'] . "'>" . $row['nome_cargo'] . "</option>";
-                }
-                mysqli_close($con);
-            ?>
-        </select>
-        <br>
-    
-        <button type="submit">Enviar</button>
+        <div class="mb-3">
+            <label class="font-1-s" for="cpf">CPF</label>
+            <input class="form-control" type="text" name="cpf" required>
+        </div>
+
+        <div class="mb-3">
+            <label class="font-1-s" for="telefone">Telefone</label>
+            <input class="form-control" type="fone" name="telefone" required>
+        </div>
+        
+        <div class="mb-3">
+            <label for="id_cargo">Cargo</label>
+                <select class="form-select" name="id_cargo" required>
+                    <?php
+                        include '../../config/conexao.php';
+                        $query = "SELECT id_cargo, nome_cargo FROM tbl_cargo";
+                        $result = mysqli_query($con, $query);
+            
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            echo "<option value='" . $row['id_cargo'] . "'>" . $row['nome_cargo'] . "</option>";
+                        }
+                        mysqli_close($con);
+                    ?>
+            </select>
+        </div>
+
+        <div class="mb-3">
+            <label class="font-1-s" for="senha">Senha</label>
+            <input class="form-control" type="password" name="senha" required>
+        </div>
+
+        <div class="form-container-button">
+            <button class='btn btn-primary' type="submit">ADICIONAR</button>
+        </div>
+
     </form>
-    
+    </div>
     <!-- fim formulario -->
 </body>
 </html>
