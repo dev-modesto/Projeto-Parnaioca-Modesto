04d41af47786e92b9e1051bfbc881cfa8aab0970
Gabriel.Modesto <gabriel_26@outlook.com.br>
2024-06-21 14:13:40 -0300

    feat: retornando informações com $_SESSION

--- app/login/include/cLogin.php
+++ app/login/include/cLogin.php
@@ -1,6 +1,5 @@
 <?php
     include __DIR__ . '/../../../config/conexao.php';
-    //estou usando o '__DIR__' para passar o caminho com base no atual. 
 
     if(isset($_POST['login'])){
 
@@ -13,23 +12,35 @@
         $verifica = mysqli_num_rows($busca_login);//verificando se exite o login. se true = existe, false = nao existe
         
         if($verifica){
-
+            //verificamos se o login existe. [existe]
             $array = mysqli_fetch_array($busca_login);
             $hash = $array['senha'];
             $dt_cadastro = $array['dt_cadastro'];
             $dt_atualizacao = $array['dt_atualizacao'];
 
-            if (password_verify($senha, $hash)){
-                if ($dt_cadastro == $dt_atualizacao){
+            if (password_verify($senha, $hash)){ //verificando a senha digitada com a senha do banco
 
-                    $mensagem = ' | Você precisa atualizar a senha.';
+                //[senha correta]
+                session_start();
+                //armazenando os dados do banco na sessão
+                $_SESSION['id'] = $array['id_funcionario'];
+                $_SESSION['nome'] = $array['nome'];
+                $_SESSION['cpf'] = $array['cpf'];
+
+                //verificando se houve atualizacção da senha. (caso nao houve, é o primeiro login do usuario)
+                if ($dt_cadastro == $dt_atualizacao){
+                    //[não houve atualizacao]                   
                     // levar para tela de atualização de senha do usuário
+                    header('location: ../login/pag_primeira_senha.php');
+                    
                 }else {
+                    //[usuario ja atualizou]
                    // echo '| Senha já atualizada. Podemos continuar | ACESSO AO SISTEMA LIBERADO!!!!';
                     header('location: ./include/pag_sucesso.php');                    
                 }
 
             }else{
+                // [senha incorreta]
                 // echo 'LOGIN OU SENHA INVÁLIDOS.';
                 $mensagem = 'Senha inválida';
             }
@@ -37,6 +48,8 @@
         }else{
             // echo 'USUARIO NAO ENCONTRADO OU SENHA INVÁLIDOS!';
             $mensagem = 'Usuário não encontrado ou senha inválida';
+            // session_abort();
+            // echo 'sessao abortada';
         }
 
     }else {
