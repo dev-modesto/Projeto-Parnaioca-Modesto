dcd25a6d22e17835234d073923d8dd75ca7fab16
dev-modesto <gabriel_26@outlook.com.br>
2024-06-21 00:52:38 -0300

    feat: passando senha como hash

--- app/funcionario/include/gFuncionario.php
+++ app/funcionario/include/gFuncionario.php
@@ -7,11 +7,11 @@
     $id_cargo = trim($_POST['id_cargo']);
     $senha = trim($_POST['cpf']);
 
-    // $hash = password_hash($senhaNoHash,PASSWORD_DEFAULT);
+    $hash = password_hash($senha,PASSWORD_DEFAULT);
 
     $sql = "INSERT INTO tbl_funcionario
         (id_funcionario,nome,cpf,telefone,id_cargo,senha) 
-        VALUES (null,'$nome','$cpf','$telefone','$id_cargo','$senha')";
+        VALUES (null,'$nome','$cpf','$telefone','$id_cargo','$hash')";
 
     if(mysqli_query($con, $sql)){
         echo "Gravado com sucesso!";
