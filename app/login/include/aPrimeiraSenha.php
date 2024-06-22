741df1a6032f8aa8ea2fe1e3067b8b52d0d2f5e6
dev-modesto <gabriel_26@outlook.com.br>
2024-06-22 15:05:42 -0300

    feat: atualização da primeira senha acesso

--- app/login/include/aPrimeiraSenha.php
+++ app/login/include/aPrimeiraSenha.php
@@ -1,30 +1,46 @@
 <?php 
     include __DIR__ . '/../../../config/conexao.php'; 
+    include './../funcao/verificaCriterioSenha.php'; 
+    
     session_start(); 
 
     if(isset($_POST['senha'])){
-
         $novaSenha = trim($_POST['senha']);
         $confirmaNovaSenha = trim($_POST['senha-confirma']);
 
         if($novaSenha == $confirmaNovaSenha){
-            echo 'senhas identicas | ';
-            echo $_SESSION['id'];
 
             $sql = 'SELECT * FROM tbl_funcionario WHERE id_funcionario = ' . $_SESSION['id'];
             $retornoConsulta = mysqli_query($con,$sql);
-            $hash = password_hash($novaSenha,PASSWORD_DEFAULT);//passando o senha como hash
+            $hash = password_hash($novaSenha,PASSWORD_DEFAULT);
+            $id = $_SESSION['id'];
 
-            if(mysqli_num_rows($retornoConsulta)){
-                echo ' | Achou o id no banco!';
-            } else {
-                echo 'nada foi encontrado';
+            if(verificaCriterioSenha($criterioSenha, $novaSenha)){
+
+                $sql2 = "UPDATE tbl_funcionario SET senha = '" . $hash . "' WHERE id_funcionario = " . $id;
+
+                if(mysqli_query($con,$sql2)){
+                    $_SESSION['update_success'] = true;
+                    ?> 
+                        <script>
+                            alert("Senha atualizada com sucesso!")
+                            window.location.href = './index.php';
+                        </script>
+                    <?php
+                    session_destroy();
+                } else {
+                    $newMensage = "Erro ao gravar" . mysqli_error($con);
+                }
+                mysqli_close($con);
+
+            }else {
+                $mensagem = 'A senha nao atendeu aos requisitos.';
             }
+            
 
         } else {
-            echo 'senhas não coincidem';
+            $mensagem = 'Senhas não coincidem';
         }
         
-        // $hash = password_hash($senhaConfirmada,PASSWORD_DEFAULT)
     }
 ?>
