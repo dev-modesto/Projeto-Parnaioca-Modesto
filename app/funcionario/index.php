3d0229fc0e2714f99a0d8e01fc223d98ab047329
dev-modesto <gabriel_26@outlook.com.br>
2024-06-24 01:07:59 -0300

    feat: tabela retornando funcionados db

--- app/funcionario/index.php
+++ app/funcionario/index.php
@@ -1,50 +1,152 @@
 <?php
     include '../../include/navbar-lateral/navbar-lateral.php';
+    include 'include/gFuncionario.php';
+
+    include __DIR__  . '/../../config/conexao.php';
+
+    $sql2=  "SELECT f.id_funcionario, f.nome, f.cpf, f.telefone, c.nome_cargo FROM tbl_funcionario f INNER JOIN tbl_cargo c ON f.id_cargo = c.id_cargo";
+    $consulta = mysqli_query($con, $sql2);
+
 ?>
-<div class="conteudo">
-
-
-    <!-- formulario envio cargo -->
-    <form class="form-container" action="include/gFuncionario.php" method="post">
-        <h1>Cadastrar Funcionario</h1>
-        <span class="separador"></span>
-        <div class="mb-3">
-            <label class="font-1-s" for="nome">Nome completo</label>
-            <input class="form-control" type="text" name="nome" required>
-        </div>
-
-        <div class="mb-3">
-            <label class="font-1-s" for="cpf">CPF</label>
-            <input class="form-control" type="text" name="cpf" required>
-        </div>
-
-        <div class="mb-3">
-            <label class="font-1-s" for="telefone">Telefone</label>
-            <input class="form-control" type="fone" name="telefone" required>
-        </div>
+    <div class="conteudo">
+        <div class="container-conteudo-principal">
         
-        <div class="mb-3">
-            <label for="id_cargo">Cargo</label>
-                <select class="form-select" name="id_cargo" required>
-                    <?php
-                        include '../../config/conexao.php';
-                        $query = "SELECT id_cargo, nome_cargo FROM tbl_cargo";
-                        $result = mysqli_query($con, $query);
-            
-                        while ($row = mysqli_fetch_assoc($result)) {
-                            echo "<option value='" . $row['id_cargo'] . "'>" . $row['nome_cargo'] . "</option>";
-                        }
-                        mysqli_close($con);
-                    ?>
-            </select>
+                <h1>Funcionarios</h1>
+                <div class="container-button">
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Novo funcionário</button>
+                </div>
+
+                <span class="separador"></span>
+                <?php if(!empty($mensagem)){ ?>  
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        <?php echo $mensagem ?>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div> 
+                <?php }else {
+                        echo '';
+                    }
+                ?>
+
+            <div class="container-tabela">
+                <table class="table table-hover text-center">
+                    <thead class="">
+                        <tr>
+                            <th scope="col">Matrícula</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">CPF</th>
+                            <th scope="col">Telefone</th>
+                            <th scope="col">Cargo</th>
+                            <th scope="col">Controle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php 
+                            while($exibe = mysqli_fetch_array($consulta)){
+                                ?>
+                                <tr>
+                                    <td><?php echo $exibe['id_funcionario']?></td>
+                                    <td><?php echo $exibe['nome']?></td>
+                                    <td><?php echo $exibe['cpf']?></td>
+                                    <td><?php echo $exibe['telefone']?></td>
+                                    <td><?php echo $exibe['nome_cargo']?></td>
+                                    <td>
+                                        <a href=""><span class="material-symbols-rounded">visibility</span></a>
+                                        <a href=""><span class="material-symbols-rounded">edit</span></a>
+                                        <a href=""><span class="material-symbols-rounded">delete</span></a>
+                                    </td>
+                                </tr>
+                                <?php
+                            }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+
+            <!-- Modal -->
+            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="staticBackdropLabel">Cadastrar funcionário</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+
+                        <!-- formulario envio cargo -->
+                        <form class="was-validated form-container" action="" method="post">
+                            <div class="mb-3">
+                                <label class="font-1-s" for="nome">Nome completo</label>
+                                <input class="form-control" type="text" name="nome" id="validationText" required>
+                                <div class="invalid-feedback">
+                                    
+                                </div>
+                            </div>
+
+                            <div class="mb-3">
+                                <label class="font-1-s" for="cpf">CPF</label>
+                                <input class="form-control" type="text" name="cpf" class="cpf" id="cpf" required>
+                                <!-- <p id="info-validaCpf"></p> -->
+                                <div class="invalid-feedback">
+                                   
+                                </div>
+                            </div>
+
+                            <div class="mb-3">
+                                <label class="font-1-s" for="telefone">Telefone</label>
+                                <input class="form-control" type="fone" name="telefone" class="telefone" id="telefone" required>
+                                <div class="invalid-feedback">
+                                    
+                                </div>
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="id_cargo">Cargo</label>
+                                <select class="form-select" name="id_cargo" required aria-label="select example">
+                                    <option value="">Selecione um cargo</option>
+                                    <?php
+                                        include '../../config/conexao.php';
+                                        $query = "SELECT id_cargo, nome_cargo FROM tbl_cargo";
+                                        $result = mysqli_query($con, $query);
+                            
+                                        while ($row = mysqli_fetch_assoc($result)) {
+                                            echo "<option value='" . $row['id_cargo'] . "'>" . $row['nome_cargo'] . "</option>";
+                                        }
+                                        mysqli_close($con);
+                                    ?>
+                                </select>
+                            </div>
+
+                            <?php if(!empty($mensagem)){ ?>  
+                                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                    <?php echo $mensagem ?>
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                </div> 
+                            <?php }else {
+                                    echo '';
+                                }
+                            ?>
+
+                            <div class="modal-footer form-container-button">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                                <button class='btn btn-primary' type="submit">Adicionar</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
         </div>
 
-        <div class="form-container-button">
-            <button class='btn btn-primary' type="submit">ADICIONAR</button>
-        </div>
-
-    </form>
     </div>
-    <!-- fim formulario -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.js" integrity="sha512-0XDfGxFliYJPFrideYOoxdgNIvrwGTLnmK20xZbCAvPfLGQMzHUsaqZK8ZoH+luXGRxTrS46+Aq400nCnAT0/w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script>
+        $('#cpf').mask('000.000.000-00', {reverse: true});
+        $('#telefone').mask('0000000000000');
+    </script>
+
 </body>
 </html>
