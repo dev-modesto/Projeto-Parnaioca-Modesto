16a62c5eac98e5708a7a08241c462996c83e81a5
Gabriel.Modesto <gabriel_26@outlook.com.br>
2024-06-25 14:56:30 -0300

    feat: atualizar funcionario

--- app/funcionario/include/aFuncionario.php
+++ app/funcionario/include/aFuncionario.php
@@ -1,24 +1,40 @@
 <?php
     include __DIR__  . '/../../../config/conexao.php';
     
-    if(isset($_GET['id'])){
 
+    if($_GET['id']){
+        echo 'clicamoes e recebemos algo';
         $id = $_GET['id'];
-        echo $id;
-
-        $sql = "SELECT * FROM tbl_funcionario WHERE id_funcionario = " . $id;
-
-        $consulta = mysqli_query($con,$sql);
-        $array = mysqli_fetch_array($consulta);
-
-        $minhaVariavel = 'trouxe';
-        header('location: ../index.php?id='.$id);
-        
-        // if($array = mysqli_fetch_array($consulta)){
-        //     echo "<pre>";
-        //     print_r($array);
-        // }
-
+    }else{
+        echo 'nada';
     };
+
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        echo "tentativa de post realizada";
+
+        // die;
+        $nome = trim($_POST['nome']);
+        $cpf = trim($_POST['cpf']);
+        $telefone = trim($_POST['telefone']);
+        $id_cargo = trim($_POST['id_cargo']);
+    
+        echo "<pre>";
+        echo $nome . "\n";
+        echo $cpf . "\n";
+        echo $telefone . "\n";
+        echo $id_cargo . "\n";
+        echo $id;
+        // die;
+        $query = "UPDATE tbl_funcionario SET nome='$nome',cpf='$cpf',telefone='$telefone',id_cargo='$id_cargo' WHERE id_funcionario = '$id'";
+        
+        if($executeUpdate = mysqli_query($con, $query)){
+            $mensagem = "Usuario atualizado com sucesso!";
+            echo "Atualizado com sucesso!";
+            header('location: ../index.php?msg=Atualizado com sucesso!');
+        }else {
+            echo "Erro ao gravar: " . mysqli_error($con);
+        };
+
+    }
 
 ?>
