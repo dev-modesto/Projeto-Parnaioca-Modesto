a9b1540344be0fc2188972ba4585d5d9472909d7
dev-modesto <gabriel_26@outlook.com.br>
2024-06-26 00:39:25 -0300

    refactor: atualizando e passando id com post

--- app/funcionario/include/aFuncionario.php
+++ app/funcionario/include/aFuncionario.php
@@ -1,39 +1,24 @@
 <?php
     include __DIR__  . '/../../../config/conexao.php';
     
-
-    if($_GET['id']){
-        echo 'clicamoes e recebemos algo';
-        $id = $_GET['id'];
-    }else{
-        echo 'nada';
-    };
-
     if($_SERVER['REQUEST_METHOD'] == "POST"){
-        echo "tentativa de post realizada";
 
-        // die;
+        $id = $_POST['id-funcionario'];
         $nome = trim($_POST['nome']);
         $cpf = trim($_POST['cpf']);
         $telefone = trim($_POST['telefone']);
         $id_cargo = trim($_POST['id_cargo']);
-    
-        echo "<pre>";
-        echo $nome . "\n";
-        echo $cpf . "\n";
-        echo $telefone . "\n";
-        echo $id_cargo . "\n";
-        echo $id;
-        // die;
+        
         $query = "UPDATE tbl_funcionario SET nome='$nome',cpf='$cpf',telefone='$telefone',id_cargo='$id_cargo' WHERE id_funcionario = '$id'";
         
         if($executeUpdate = mysqli_query($con, $query)){
             $mensagem = "Usuario atualizado com sucesso!";
-            echo "Atualizado com sucesso!";
             header('location: ../index.php?msg=Atualizado com sucesso!');
         }else {
             echo "Erro ao gravar: " . mysqli_error($con);
         };
+
+        mysqli_close($con);
 
     }
 
