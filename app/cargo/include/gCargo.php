3c08988a43f1be673bcd03c54a2a9ac47c188b77
Gabriel.Modesto <gabriel_26@outlook.com.br>
2024-06-28 09:55:54 -0300

    feat: chamada função converter monetário

--- app/cargo/include/gCargo.php
+++ app/cargo/include/gCargo.php
@@ -1,15 +1,17 @@
 <?php
     include __DIR__  . '/../../../config/conexao.php';
+    include './../../funcao/converter.php';
 
     if(isset($_POST['cargo'])){
         
         $cargo = trim($_POST['cargo']);
         $salario = trim($_POST['salario']);
         $idSetor = $_POST['idSetor'];
+        $salarioConvertido = converterMonetario($salario);
 
         $stmt = mysqli_prepare($con, "INSERT INTO tbl_cargo(id_cargo, nome_cargo, salario, id_setor) VALUES (NULL, ?, ?, ?)");
 
-        mysqli_stmt_bind_param($stmt, 'sdi',$cargo,$salario,$idSetor);
+        mysqli_stmt_bind_param($stmt, 'sdi',$cargo,$salarioConvertido,$idSetor);
 
         if(mysqli_stmt_execute($stmt)){
             header('location: ../index.php?msg=Adicionado com sucesso!');
