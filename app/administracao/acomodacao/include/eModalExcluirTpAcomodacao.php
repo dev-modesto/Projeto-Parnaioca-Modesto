2bb360de10676ebe201a8d7bc495a8690f850bb1
dev-modesto <gabriel_26@outlook.com.br>
2024-07-02 20:48:37 -0300

    feat: arquivo modal excluir tipo acomodação

---
+++ app/administracao/acomodacao/include/eModalExcluirTpAcomodacao.php
@@ -0,0 +1,31 @@
+<?php
+
+    if(isset($_POST['click-excluir-tp-acomodacao'])){
+        $idTpAcomodacao = $_POST['idTpAomocadao'];
+    }
+
+?>
+
+<div class="modal fade" id="modalExcluir" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="modalExcluir" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header modal-header-second">
+                <span class="icone-alerta-modal material-symbols-rounded">error</span>
+            </div>
+            <div class="modal-body body-alerta-modal">
+                <p><strong>Você tem certeza que deseja remover?</strong> <br>
+                    Esta é uma ação irreversível.
+                </p>
+            </div>
+
+            <form class="was-validated form-container" action="include/eTpAcomodacao.php" method="post">
+                <input type="text" name="idTpAcomodacao" id="idTpAcomodacao" value="<?php echo $idTpAcomodacao ?>" hidden>
+                <div class="modal-footer form-container-button">
+                    <button type="button" class="btn btn-secondary btn-modal-cancelar" data-bs-dismiss="modal">Cancelar</button>
+                    <button class='btn btn-modal-excluir' type="submit">Excluir</button>
+                </div>
+            </form>
+            
+        </div>
+    </div>
+</div>
