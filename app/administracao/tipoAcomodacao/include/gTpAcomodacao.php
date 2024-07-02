6da58370fa68cebe5d3c6e190f511344900b1761
dev-modesto <gabriel_26@outlook.com.br>
2024-07-02 17:48:08 -0300

    fix: inserindo 'trim' no nome do tipo de acomodação

--- app/administracao/tipoAcomodacao/include/gTpAcomodacao.php
+++ app/administracao/tipoAcomodacao/include/gTpAcomodacao.php
@@ -3,13 +3,12 @@
     include ARQUIVO_CONEXAO;
 
     if($_SERVER['REQUEST_METHOD'] == "POST"){
-        $nomeTpAcomodacao = $_POST['nome-tp-acomodacao'];
+        $nomeTpAcomodacao = trim($_POST['nome-tp-acomodacao']);
 
         $sql = mysqli_prepare($con, "INSERT INTO tbl_tp_acomodacao (id_tp_acomodacao, nome_tp_acomodacao) VALUES (null, ?)");
         mysqli_stmt_bind_param($sql, 's', $nomeTpAcomodacao);
 
         if(mysqli_stmt_execute($sql)){
-            // echo 'gravado com sucesso!';
             header('location: ../index.php?msg=Adicionado com sucesso!');
         } else {
             echo "Error ao gravar" . mysqli_error($con);
