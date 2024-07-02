dc16487b9ff10789ae206e7cd673bd03f6421fcd
dev-modesto <gabriel_26@outlook.com.br>
2024-07-02 20:38:47 -0300

    fix: link plugin idioma pt-br table

--- js/table.js
+++ js/table.js
@@ -1,9 +1,10 @@
-$(document).ready( function () {
-    $('#myTable').DataTable();
-} );
-
 var table = new DataTable('#myTable', {
     language: {
         url: '//cdn.datatables.net/plug-ins/2.0.8/i18n/pt-BR.json',
     },
 });
+
+$(document).ready( function () {
+    $('#myTable').DataTable();
+} );
+
