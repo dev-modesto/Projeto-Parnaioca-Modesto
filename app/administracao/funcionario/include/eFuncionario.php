35b872d0120642b1f778cd684a69a4d6ea6d1128
dev-modesto <gabriel_26@outlook.com.br>
2024-07-07 00:01:48 -0300

    security: chamando função exluir nivel de acesso

--- app/administracao/funcionario/include/eFuncionario.php
+++ app/administracao/funcionario/include/eFuncionario.php
@@ -1,11 +1,14 @@
 <?php
     include $_SERVER['DOCUMENT_ROOT'] . '/Projeto-Parnaioca-Modesto/config/config.php';
+    include $_SERVER['DOCUMENT_ROOT'] . '/Projeto-Parnaioca-Modesto/app/funcao/funcaoSql.php';
     include ARQUIVO_CONEXAO;
 
     if(isset($_POST['idFuncionario'])){
         $id = $_POST['idFuncionario'];
         // echo 'recebemos o id: ' .  $id;
+        excluirNivelAcessoPadrao($con, $id);
         $sql = "DELETE FROM tbl_funcionario where id_funcionario = '$id'";
+
         if(mysqli_query($con, $sql)){
             $mensagem = "Funcionário exluido com sucesso!";
             header('location: ../index.php?msg=Deletado com sucesso!');
