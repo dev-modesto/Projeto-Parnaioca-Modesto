f18b987b506320551a51a7d1565c9d0d731a1aef
dev-modesto <gabriel_26@outlook.com.br>
2024-07-07 16:04:50 -0300

    security: aplicando verificação de adm

--- config/seguranca.php
+++ config/seguranca.php
@@ -14,4 +14,18 @@
         header('location: ../../app/login/');
     }
 
+    function segurancaAdm($con, $id){
+        $sqlAcesso = "SELECT * FROM tbl_acesso_area WHERE id_funcionario = '$id'";
+        $consultaAcesso = mysqli_query($con, $sqlAcesso);
+        $arrayAcesso = mysqli_fetch_array($consultaAcesso);
+
+        $idAcesso = $arrayAcesso['administracao'];
+
+        if ($idAcesso != 1) {
+            // verificando se não é administrador
+            header('location:' .BASE_URL .'/app/cliente/index.php');
+        }
+
+    }
+
 ?>
