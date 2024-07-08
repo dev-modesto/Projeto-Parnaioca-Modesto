42ebc23531a528ed230c9788b4a902bafebee24d
dev-modesto <gabriel_26@outlook.com.br>
2024-07-08 20:53:45 -0300

    feat: index log de operação

---
+++ app/administracao/logOperacao/index.php
@@ -0,0 +1,150 @@
+<?php
+    $tituloPagina = "Administração";
+    $pagina = "Logs de Operações";
+    $grupoPagina = "Logs geral";
+    include $_SERVER['DOCUMENT_ROOT'] . '/Projeto-Parnaioca-Modesto/config/base.php';
+
+    if (session_status() == PHP_SESSION_ACTIVE) {
+        $idLogado = $_SESSION['id'];
+        segurancaAdm($con, $idLogado);
+    }
+    
+    $sql = 
+        "SELECT
+            l.id_log_op, 
+            l.id_funcionario,
+            f.nome,
+            l.nome_tbl, 
+            l.id_registro, 
+            l.tp_operacao, 
+            l.descricao, 
+            l.dt_ocorrencia 
+        FROM tbl_log_operacao l
+        INNER JOIN tbl_funcionario f
+        ON l.id_funcionario = f.id_funcionario
+    ";
+
+    $consulta = mysqli_query($con, $sql);
+
+?>
+    
+    <!DOCTYPE html>
+    <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Administração | Log de Operações</title>
+        <!-- link bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <!-- meu css -->
+        <link rel="stylesheet" href="../../../css/style.css">
+        <!-- meus icons -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,1,0" />
+
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@1,900&family=Poppins:wght@200;300;400;600;700&family=Roboto:wght@200;300;400;500&display=swap" rel="stylesheet">
+        
+        <!-- link css datatable -->
+        <link rel="stylesheet" href="https://cdn.datatables.net/2.0.8/css/dataTables.dataTables.css" />
+    
+    
+    </head>
+    <body>
+
+    <div class="conteudo">
+        <div class="container-conteudo-principal">
+
+            <?php
+                if(isset($_GET['msg'])){
+                    $msg = $_GET['msg'];
+                    echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+                            '. $msg .'
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>';
+                }
+            
+            ?>
+
+            <?php
+                if(isset($_GET['msgInvalida'])){
+                    $msg = $_GET['msgInvalida'];
+                    echo '<div class="alert alert-danger  alert-dismissible fade show" role="alert">
+                            '. $msg .'
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>';
+                }
+            
+            ?>
+
+            <span class="separador"></span>
+
+            <!-- Tabela -->
+            <div class="container-tabela">
+
+                <table id="myTable" class="table nowrap order-column table-hover text-left">
+                    <thead class="">
+                        <tr>
+                            <!-- <th scope="col">Nº</th> -->
+                            <th scope="col">#ID log</th>
+                            <th scope="col">ID funcionario</th>
+                            <th scope="col">Nome funcionário</th>
+                            <th scope="col">Tabela</th>
+                            <th scope="col">ID registro tabela</th>
+                            <th scope="col">Tipo operação</th>
+                            <th scope="col">Descrição</th>
+                            <th scope="col">Data ocorrência</th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-group-divider">
+                        <?php 
+                            $nroLinha = 1;
+                            while($exibe = mysqli_fetch_array($consulta)){
+                                    $id = $exibe['id_log_op'];
+                                ?>
+                                <tr>
+                                    <!-- <td class="numero-linha"><?php echo $nroLinha++; ?></td> -->
+                                    <td class="id"><?php echo $exibe['id_log_op']?></td>
+                                    <td><?php echo $exibe['id_funcionario']?></td>
+                                    <td><?php echo $exibe['nome']?></td>
+                                    <td><?php echo $exibe['nome_tbl']?></td>
+                                    <td><?php echo $exibe['id_registro']?></td>
+                                    <td><?php echo $exibe['tp_operacao']?></td>
+                                    <td><?php echo $exibe['descricao']?></td>
+                                    <td><?php echo $exibe['dt_ocorrencia']?></td>
+                                </tr>
+                                <?php
+                            }
+                        ?>
+                    </tbody>
+
+                </table>
+            </div>
+
+            <div class="modalEditarAcessoArea">
+            </div>
+
+        </div>
+
+    </div>
+
+    <?php
+        include __DIR__ . '/../../../include/footer.php';
+    ?>
+
+    <script src="<?php echo BASE_URL ?>/js/modal.js"></script>
+    <script src="<?php echo BASE_URL ?>/js/table.js"></script>
+
+
+    <script>
+
+        var legenda = document.querySelectorAll('.legenda-acesso').forEach(function (element) {
+            if (element.textContent.trim() == 1 ) {
+                element.innerHTML = 'Sim';
+                element.classList.add('sim');
+            } else {
+                element.innerHTML = 'Não';
+                element.classList.add('não');
+            }
+        })
+
+    </script>
