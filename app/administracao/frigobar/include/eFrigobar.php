d8988c6975321586f333dff8d525d1a98bb0e4a4
dev-modesto <gabriel_26@outlook.com.br>
2024-07-08 22:34:52 -0300

    feat: log de operação frigobar (atualização, exclusao e inserção)

--- app/administracao/frigobar/include/eFrigobar.php
+++ app/administracao/frigobar/include/eFrigobar.php
@@ -1,13 +1,26 @@
 <?php
     include $_SERVER['DOCUMENT_ROOT'] . '/Projeto-Parnaioca-Modesto/config/config.php';
     include ARQUIVO_CONEXAO;
+    include ARQUIVO_FUNCAO_SQL;
+
+    session_start();
+
+    if (session_status() == PHP_SESSION_ACTIVE) {
+        $idLogado = $_SESSION['id'];
+    }
 
     if(isset($_POST['id-frigobar'])){
         $id = $_POST['id-frigobar'];
-        // echo 'recebemos o id: ' .  $id;
-        // die();
         $sql = "DELETE FROM tbl_frigobar where id_frigobar = '$id'";
+
         if(mysqli_query($con, $sql)){
+            // log operações
+                $nomeTabela = 'tbl_frigobar';
+                $idRegistro = $id;
+                $tpOperacao = 'exclusao';
+                $descricao = 'Frigobar excluído ID: ' . $idRegistro;
+                logOperacao($con,$idLogado,$nomeTabela,$idRegistro,$tpOperacao,$descricao);
+            // 
             $mensagem = "Frigobar exluido com sucesso!";
             header('location: ../index.php?msg=Deletado com sucesso!');
         
