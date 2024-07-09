785b992ca6657406deab22d5643830874bf6405f
dev-modesto <gabriel_26@outlook.com.br>
2024-07-08 22:35:37 -0300

    feat: log de operação tipo acomodação (atualização, exclusao e inserção)

--- app/administracao/tipoAcomodacao/include/aTpAcomodacao.php
+++ app/administracao/tipoAcomodacao/include/aTpAcomodacao.php
@@ -1,6 +1,13 @@
 <?php
     include $_SERVER['DOCUMENT_ROOT'] . '/Projeto-Parnaioca-Modesto/config/config.php';
     include ARQUIVO_CONEXAO;
+    include ARQUIVO_FUNCAO_SQL;
+
+    session_start();
+
+    if (session_status() == PHP_SESSION_ACTIVE) {
+        $idLogado = $_SESSION['id'];
+    }
 
     if(isset($_POST['idTpAcomodacao'])){
         $id = $_POST['idTpAcomodacao'];
@@ -10,6 +17,13 @@
         mysqli_stmt_bind_param($stmt, 'si', $nomeTpAcomodacao, $id);
 
         if(mysqli_stmt_execute($stmt)){
+            // log operações
+                $nomeTabela = 'tbl_tp_acomodacao';
+                $idRegistro = $id;
+                $tpOperacao = 'atualizacao';
+                $descricao = 'Tipo acomodação atualizada ID: ' . $idRegistro;
+                logOperacao($con,$idLogado,$nomeTabela,$idRegistro,$tpOperacao,$descricao);
+            // 
             header('location: ../index.php?msg=Atualizado com sucesso!');
         } else {
             echo "Erro ao gravar: " . mysqli_error($con);
