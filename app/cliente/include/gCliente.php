289b229a8c3c21870f3156803b04253c763f3fd3
Gabriel.Modesto <gabriel_26@outlook.com.br>
2024-07-10 11:55:30 -0300

    feat: verificando e tratando se cpf e email já foi cadastrado

--- app/cliente/include/gCliente.php
+++ app/cliente/include/gCliente.php
@@ -25,55 +25,90 @@
 
         if(strlen($cpf) < 14){
             header('location: ../index.php?msgInvalida=Cpf inválido. Favor, preencha corretamente.');
+        
         } else {
 
-            $sql = 
-                mysqli_prepare(
-                $con, 
-                "INSERT INTO tbl_cliente (
-                    id_cliente, 
-                    nome, 
-                    dt_nascimento, 
-                    cpf, 
-                    email, 
-                    telefone, 
-                    estado, 
-                    cidade, 
-                    id_funcionario, 
-                    id_status) 
-                VALUES (null, ?, ?, ?, ?, ?, ?, ?, ?, ? )"
-            );
+            $sqlVerifica = mysqli_prepare($con, "SELECT cpf, email FROM tbl_cliente WHERE cpf = ? OR email = ? ");
+            mysqli_stmt_bind_param($sqlVerifica, "ss", $cpf, $email);
+            mysqli_stmt_execute($sqlVerifica);
+            $result = mysqli_stmt_get_result($sqlVerifica);
+            
+            if (mysqli_num_rows($result) > 0) {
 
-            mysqli_stmt_bind_param(
-                $sql,
-                "sssssssii",
-                $nome, 
-                $dataNascimento, 
-                $cpf, 
-                $email, 
-                $telefone,
-                $estado, 
-                $cidade, 
-                $idFuncionario, 
-                $idStatus
-            );
-    
-            if(mysqli_stmt_execute($sql)){
-                $idCliente = mysqli_insert_id($con);
+                $array = mysqli_fetch_assoc($result);
+                $msg = '';
+                
+                if($array['cpf'] == $cpf ){
 
-                // log operações
-                    $nomeTabela = 'tbl_cliente';
-                    $idRegistro = $idCliente;
-                    $tpOperacao = 'insercao';
-                    $descricao = 'Cliente adicionado ID: ' . $idCliente;
-                    logOperacao($con,$idLogado,$nomeTabela,$idRegistro,$tpOperacao,$descricao);
-                // 
-                header('location: ../index.php?msg=Adicionado com sucesso!');
+                    echo 'este cpf ja foi cadastrado';
+                    $msg .= "Este CPF já foi cadastrado anteriormente.";
+                    mysqli_close($con);
+                    header('location: ../index.php?msgInvalida=' . $msg);
+                    
+                } elseif ($array['email'] == $email ){
+
+                    echo 'este email ja foi cadastrado';
+                    $msg .= "Este e-mail já foi cadastrado anteriormente.";
+                    mysqli_close($con);
+                    header('location: ../index.php?msgInvalida=' . $msg);
+                
+                } 
+
             } else {
-                echo "Error ao gravar" . mysqli_error($con);
+
+                $sql = 
+                    mysqli_prepare(
+                    $con, 
+                    "INSERT INTO tbl_cliente (
+                        id_cliente, 
+                        nome, 
+                        dt_nascimento, 
+                        cpf, 
+                        email, 
+                        telefone, 
+                        estado, 
+                        cidade, 
+                        id_funcionario, 
+                        id_status) 
+                    VALUES (null, ?, ?, ?, ?, ?, ?, ?, ?, ? )"
+                );
+
+                mysqli_stmt_bind_param(
+                    $sql,
+                    "sssssssii",
+                    $nome, 
+                    $dataNascimento, 
+                    $cpf, 
+                    $email, 
+                    $telefone,
+                    $estado, 
+                    $cidade, 
+                    $idFuncionario, 
+                    $idStatus
+                );
+
+                if(mysqli_stmt_execute($sql)){
+                    $idCliente = mysqli_insert_id($con);
+
+                    // log operações
+                        $nomeTabela = 'tbl_cliente';
+                        $idRegistro = $idCliente;
+                        $tpOperacao = 'insercao';
+                        $descricao = 'Cliente adicionado ID: ' . $idCliente;
+                        logOperacao($con,$idLogado,$nomeTabela,$idRegistro,$tpOperacao,$descricao);
+                    // 
+                    header('location: ../index.php?msg=Adicionado com sucesso!');
+
+                } else {
+                    echo "Error ao gravar" . mysqli_error($con);
+                
+                }
+
+                mysqli_close($con);
+
             }
-            mysqli_close($con);
-        }
+
+        } 
 
     } else {
         $mensagem = "";
