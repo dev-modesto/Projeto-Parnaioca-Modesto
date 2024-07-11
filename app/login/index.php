fec7ce5a0c3679554f861ed77ee9d25357334afa
Gabriel.Modesto <gabriel_26@outlook.com.br>
2024-07-11 10:00:41 -0300

    feat: botão ver senha

--- app/login/index.php
+++ app/login/index.php
@@ -30,16 +30,17 @@
                         <span class="separador-login"></span>
                         <div class="mb-3">
                             <label class="label-login" for="form-label login">Matrícula</label>
-                            <div>
+                            <div class="container-input-login">
                                 <span class="icone-login icone-matricula"></span>
                                 <input class="input-login form-control " type="text" name="login" required>
                             </div>
                         </div>
                         <div class="mb-3">
                             <label class="label-login" for="form-label senha">Senha</label>
-                            <div>
+                            <div class="container-input-login">
                                 <span class="icone-login icone-senha"></span>
-                                <input class="input-login form-control" type="password" name="senha" required>
+                                <input class="input-login form-control input-senha" type="password" name="senha" required>
+                                <span class="icone-login icone-ver-senha"></span>
                             </div>
                         </div>
 
@@ -72,4 +73,27 @@
     </main>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <script src="./../../js/senha.js"></script>
+       <?php
+       include __DIR__ . '/../../include/footer.php';
+       ?>
+        <script>
+
+            var iconVerSenha = document.querySelector('.icone-ver-senha');
+
+            iconVerSenha.addEventListener('click', function() {
+                var inputSenha = document.querySelector('.input-senha');
+
+                if (inputSenha.type === 'password') {
+                    inputSenha.type = 'text';
+                    iconVerSenha.classList.add('visible');
+                } else {
+                    inputSenha.type = 'password';
+                    iconVerSenha.classList.remove('visible');
+                }
+                
+            })
+
+        </script>
+
 </body>
