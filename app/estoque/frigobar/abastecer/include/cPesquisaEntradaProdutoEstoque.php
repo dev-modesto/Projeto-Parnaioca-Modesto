a83b7d935aaeb44e07ce8c72f9417e41eff8c08b
dev-modesto <gabriel_26@outlook.com.br>
2024-07-16 00:15:07 -0300

    feat: consulta pesquisa/verifica entrada do produto no estoque

---
+++ app/estoque/frigobar/abastecer/include/cPesquisaEntradaProdutoEstoque.php
@@ -0,0 +1,60 @@
+<?php
+    include $_SERVER['DOCUMENT_ROOT'] . '/Projeto-Parnaioca-Modesto/config/config.php';
+    include ARQUIVO_CONEXAO;
+    include ARQUIVO_FUNCAO_SQL;
+    include PASTA_FUNCOES . '/converter.php';
+
+    session_start();
+
+    if (session_status() == PHP_SESSION_ACTIVE) {
+        $idLogado = $_SESSION['id'];
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        $idSku = $_POST['id-sku'];
+
+        $sqlInner = 
+                mysqli_prepare(
+                $con, 
+                "SELECT 
+                e.id_item, 
+                e.id_sku, 
+                i.nome_item,
+                e.quantidade 
+                FROM tbl_entrada_item_estoque e
+                INNER JOIN tbl_item i
+                ON e.id_item = i.id_item
+                WHERE e.id_sku = ? "
+        );
+
+
+        mysqli_stmt_bind_param($sqlInner, "i", $idSku);
+        mysqli_stmt_execute($sqlInner);
+        $result = mysqli_stmt_get_result($sqlInner);
+
+        if (mysqli_num_rows($result) > 0) {
+            $array = mysqli_fetch_assoc($result);
+            
+            $idItem = $array['id_item'];
+            $nomeItem = $array['nome_item'];
+
+            $response = [
+                'idItem' => $idItem,
+                'nomeItem' => $nomeItem
+            ];
+    
+            header('Content-Type: application/json');
+            echo json_encode($response);
+
+        } else {
+            echo "";
+        }
+        mysqli_close($con);
+        
+    } else {
+        echo "";
+
+    }
+
+?>
