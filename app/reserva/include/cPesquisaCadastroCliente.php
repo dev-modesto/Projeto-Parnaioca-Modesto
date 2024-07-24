f814046e1c37fe33f881458156ecd2fd3ffdb569
dev-modesto <gabriel_26@outlook.com.br>
2024-07-23 23:40:48 -0300

    feat: consulta cadastro de cliente

---
+++ app/reserva/include/cPesquisaCadastroCliente.php
@@ -0,0 +1,45 @@
+<?php
+    include $_SERVER['DOCUMENT_ROOT'] . '/Projeto-Parnaioca-Modesto/config/config.php';
+    include ARQUIVO_CONEXAO;
+    include ARQUIVO_FUNCAO_SQL;
+
+    session_start();
+
+    if (session_status() == PHP_SESSION_ACTIVE) {
+        $idLogado = $_SESSION['id'];
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        
+        $cpfCliente = $_POST['cpf-cliente'];
+
+        $sqlVerifica = mysqli_prepare($con, "SELECT id_cliente, nome FROM tbl_cliente WHERE cpf = ? ");
+        mysqli_stmt_bind_param($sqlVerifica, "s", $cpfCliente);
+        mysqli_stmt_execute($sqlVerifica);
+        $resultado = mysqli_stmt_get_result($sqlVerifica);
+
+        if (mysqli_num_rows($resultado) > 0 ) {
+            $array = mysqli_fetch_assoc($resultado);
+            $idCliente = $array['id_cliente'];
+            $nomeCliente = $array['nome'];
+
+            $response = [
+                'idCliente' => $idCliente,
+                'nomeCliente' => $nomeCliente
+            ];
+    
+            header('Content-Type: application/json');
+            echo json_encode($response);
+            
+        } else {
+            echo "";
+        }
+
+        mysqli_close($con);
+        
+    } else {
+        echo "";
+
+    }
+
+?>
