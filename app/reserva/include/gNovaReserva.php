304f1b34f65f85f8f70c6f2ad16bdfb14360d1dc
Gabriel.Modesto <gabriel_26@outlook.com.br>
2024-07-24 14:54:27 -0300

    feat: [pre] gravar nova reserva

--- app/reserva/include/gNovaReserva.php
+++ app/reserva/include/gNovaReserva.php
@@ -1,6 +1,8 @@
 <?php
     include $_SERVER['DOCUMENT_ROOT'] . '/Projeto-Parnaioca-Modesto/config/config.php';
     include ARQUIVO_CONEXAO;
+    include ARQUIVO_FUNCAO_SQL;
+    include PASTA_FUNCOES . "/converter.php";
 
     session_start();
 
@@ -10,21 +12,44 @@
 
     if($_SERVER['REQUEST_METHOD'] == "POST") {
         
-        $cpf = $_POST['cpf'];
         $idAcomodacao = $_POST['id-acomodacao'];
         $idCliente = $_POST['id-cliente'];
-        $nome = $_POST['nome'];
         $totalHospede = $_POST['total-hospede'];
-        $acomodacao = $_POST['acomodacao'];
-        $numeroAcomodacao = $_POST['numero-acomodacao'];
-        $dataInicio = $_POST['data-inicio'];
-        $dataFim = $_POST['data-final'];
-        $valorDiaria = $_POST['valor-diaria'];
-        $valorReservaTotal = $_POST['valor-reserva-total'];
+        $dataCheckIn = $_POST['data-inicio'];
+        $dataCheckOut = $_POST['data-final'];
+        $horarioCheckInPadrao = "13:00";
+        $horarioCheckOutPadrao = "11:00";
+
+        $dateTimeCheckIn = new DateTime($dataCheckIn);
+        $dateTimeCheckOut = new DateTime($dataCheckOut);
+        $intervalo = $dateTimeCheckIn->diff($dateTimeCheckOut);
+        $qntNoites = $intervalo->days;
+
+        $dataHorarioCheckIn = ($dataCheckIn ." ".  $horarioCheckInPadrao);
+        $dataHorarioCheckOut = ($dataCheckOut ." ". $horarioCheckOutPadrao);
         
-        echo "<pre>";
-        $arry = [$idAcomodacao, $idCliente, $cpf, $nome, $totalHospede, $acomodacao, $numeroAcomodacao, $dataInicio, $dataFim, $valorDiaria, $valorReservaTotal];
-        print_r($arry);
+        $valorEntrada = $_POST['valor-entrada'];
+        $valorEntradaConvertido = converterMonetario($valorEntrada);
+        
+        $retornoInfoAcomodacao = consultaInfoAcomodacao($con, 0, $idAcomodacao);
+        $arrayInfoAcomodacao = mysqli_fetch_assoc($retornoInfoAcomodacao);
+        $valorDiaria = $arrayInfoAcomodacao['valor'];
+        
+        $valorReservaTotal = $valorDiaria * $qntNoites;
+
+        $array = [
+            "ID acomodacao: ". $idAcomodacao,
+            "ID cliente: " . $idCliente,
+            "Total hospedes: " . $totalHospede,
+            "Data check-in: " . $dataHorarioCheckIn,
+            "Data check-out: " .  $dataHorarioCheckOut,
+            "Valor diaria: " . $valorDiaria,
+            "Valor entrada: " . $valorEntradaConvertido, 
+            "Valor total reserva: " . $valorReservaTotal, 
+            "ID logado: " . $idLogado
+        ];
+        print_r($array);
+
 
     }   
 
