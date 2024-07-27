d38c1b4c7ef8eb6480f20cceab696ff57498f9cc
dev-modesto <gabriel_26@outlook.com.br>
2024-07-27 17:04:48 -0300

    refactor: remoção da funcão diaSemana

--- app/funcao/funcaoSql.php
+++ app/funcao/funcaoSql.php
@@ -142,22 +142,4 @@
         return $acomodacaoReservada;
     }
 
-    function diaSemanaPtbr($diaSemanaIngles){
-        switch ($diaSemanaIngles) {
-            case 'Monday':
-                return "Segunda-feira";
-            case 'Tuesday':
-                return "Terça-feira";
-            case 'Wednesday':
-                return "Quarta-feira";
-            case 'Thursday':
-                return "Quinta-feira";
-            case 'Friday':
-                return "Sexta-feira";
-            case 'Saturday':
-                return "Sábado";
-            case 'Sunday':
-                return "Domingo";
-        }
-    }
 ?>
