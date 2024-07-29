9aecd1d4ea1e9be9537f37d642999303fe055f9a
Gabriel.Modesto <gabriel_26@outlook.com.br>
2024-07-29 13:52:39 -0300

    feat: iniciando transação e atualizando valor total da reserva

--- app/reserva/include/gRealizarPagamento.php
+++ app/reserva/include/gRealizarPagamento.php
@@ -9,31 +9,43 @@
 
     if (session_status() == PHP_SESSION_ACTIVE) {
         $idLogado = $_SESSION['id'];
-        $idReservaSessao = $_SESSION['id-reserva'];
+        // $idReservaSessao = $_SESSION['id-reserva'];
     }
     
     if (isset($_POST['id-reserva'])) {
         $idReservaPost = $_POST['id-reserva'];
         $idFormaPagamento = $_POST['id-forma-pagamento'];
         $novoPagamento = $_POST['valor'];
-        $array = consultaInfoReserva($con, $idReservaPost);
+        // $array = consultaInfoReserva($con, $idReservaPost);
 
         $novoPagamentoConvertido = converterMonetario($novoPagamento);
 
         if(is_numeric($idReservaPost)) {
             $idReserva = intval($idReservaPost);
-                
+            $consultaTotalPago = consultaTotalPagamentoReserva($con, $idReserva);
+            $totalPagoAnteriormente = $consultaTotalPago['valor_total'];
+            $totalPagoMomento = $totalPagoAnteriormente + $novoPagamentoConvertido;
+
+            mysqli_begin_transaction($con);
+            
             try {
                 
                 $sql = mysqli_prepare($con, "INSERT INTO tbl_pagamento(id_reserva, valor, id_metodo_pag) VALUES(?,?,?)");
-                mysqli_stmt_bind_param($sql, 'idi', $idReservaPost, $novoPagamentoConvertido, $idFormaPagamento);
+                mysqli_stmt_bind_param($sql, 'idi', $idReserva, $novoPagamentoConvertido, $idFormaPagamento);
                 mysqli_stmt_execute($sql);
+
+                $sqlUpdate = mysqli_prepare($con, "UPDATE tbl_reserva SET total_pago = $totalPagoMomento WHERE id_reserva = ? ");
+                mysqli_stmt_bind_param($sqlUpdate, 'd', $idReserva);
+                mysqli_stmt_execute($sqlUpdate);
 
                 $mensagem = "Pagamento realizado com sucesso!";
                 header('location: ../index.php?msg=' . $mensagem);
+                
+                mysqli_commit($con);
                 mysqli_close($con);
                 
             } catch (Exception $e) {
+                mysqli_rollback($con);
                 $mensagem = "Houve um problema ao efetuar o pagamento.";
                 header('location: ../index.php?msg=' . $mensagem);
             }
