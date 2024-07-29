afc3e7fd0f4a61a88c886d43a4d0e97b6e0d382b
Gabriel.Modesto <gabriel_26@outlook.com.br>
2024-07-29 11:41:09 -0300

    refactor: tratando e inserindo informação na tabela de pagamentos

--- app/reserva/include/gRealizarPagamento.php
+++ app/reserva/include/gRealizarPagamento.php
@@ -16,31 +16,35 @@
         $idReservaPost = $_POST['id-reserva'];
         $idFormaPagamento = $_POST['id-forma-pagamento'];
         $novoPagamento = $_POST['valor'];
-
         $array = consultaInfoReserva($con, $idReservaPost);
 
-        $totalPago = $array['total_pago'];
-
         $novoPagamentoConvertido = converterMonetario($novoPagamento);
-        $novoPagamentoTratado = floatval($novoPagamentoConvertido);
-        $total = $totalPago + $novoPagamentoTratado;
 
+        if(is_numeric($idReservaPost)) {
+            $idReserva = intval($idReservaPost);
+                
+            try {
+                
+                $sql = mysqli_prepare($con, "INSERT INTO tbl_pagamento(id_reserva, valor, id_metodo_pag) VALUES(?,?,?)");
+                mysqli_stmt_bind_param($sql, 'idi', $idReservaPost, $novoPagamentoConvertido, $idFormaPagamento);
+                mysqli_stmt_execute($sql);
 
-        $sql = mysqli_prepare($con, "UPDATE tbl_reserva SET total_pago = ? WHERE id_reserva = ?");
-        mysqli_stmt_bind_param($sql, 'di', $total, $idReservaPost);
-
-        if(mysqli_stmt_execute($sql)) {
-            $mensagem = "Pagamento realizado com sucesso!";
-            header('location: ../index.php?msg=' . $mensagem);
-            mysqli_close($con);
+                $mensagem = "Pagamento realizado com sucesso!";
+                header('location: ../index.php?msg=' . $mensagem);
+                mysqli_close($con);
+                
+            } catch (Exception $e) {
+                $mensagem = "Houve um problema ao efetuar o pagamento.";
+                header('location: ../index.php?msg=' . $mensagem);
+            }
 
         } else {
-            echo "Ocorreu um erro na operação. " . mysqli_error($con);
+            $mensagem = "Não foi possível realizar o pagamento.";
+            header('location: ../index.php?msg=' . $mensagem);
         }
      
     } else {
         $msg = "O ID da reserva não foi enviado.";
         header('location: ../index.php?msgInvalida=' . $msg);
-        die();
     }
 ?>
