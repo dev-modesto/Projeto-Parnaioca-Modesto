132e8a2f40ffb4f1c6ff7244743b3ba4a96c5734
dev-modesto <gabriel_26@outlook.com.br>
2024-07-30 00:51:10 -0300

    feat: modal saida item do frigobar

---
+++ app/reserva/include/cModalSaidaItemFrigobar.php
@@ -0,0 +1,93 @@
+<?php
+    include $_SERVER['DOCUMENT_ROOT'] . '/Projeto-Parnaioca-Modesto/config/config.php';
+    include ARQUIVO_CONEXAO;
+    include ARQUIVO_FUNCAO_SQL;
+
+    if(isset($_POST['click-botao-nova-saida-frigobar'])){
+        $idReserva = $_POST['id-reserva'];
+        $idFrigobar = $_POST['id-frigobar'];
+        $idItem = $_POST['id-item'];
+
+        $arrayFrigobar = consultaInfoFrigobar($con, $idFrigobar, 0);
+        $nomeFrigobar = $arrayFrigobar['nome_frigobar'];
+
+        $arrayItem = totalItensEspecificoFrigobar($con, $idItem, $idFrigobar);
+        $nomeItem = $arrayItem['nome_item'];
+        $precoUnit = $arrayItem['preco_unit'];
+        $idAcomodacao = $arrayItem['id_acomodacao'];
+        $totalItensDiponivel = $arrayItem['total_disponivel'];
+    }
+
+?>
+
+    <!-- Modal cadastrar informações -->
+    <div class="modal fade" id="modalSaidaItemFrigobar" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="modalSaidaItemFrigobar" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="modalSaidaItemFrigobar"><?php echo $nomeFrigobar ?></h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+
+                <!-- formulario envio -->
+                <form class=" form-container" action="gSaidaItemFrigobar.php" method="post">
+                    <input class="form-control" type="text" name="id-frigobar" id="id-frigobar" value="<?php echo $idFrigobar?>" hidden required >
+                    <input class="form-control" type="text" name="id-acomodacao" id="id-acomodacao" value="<?php echo $idAcomodacao?>" hidden  required >
+                    <input class="form-control" type="text" name="id-item-frigobar" id="id-item-frigobar" value="" hidden required >
+
+                    <div class="row mb-3">
+                        
+                        <div class="col-md-8">
+                            <label class="font-1-s" for="nome-produto">Nome produto</label>
+                            <input class="form-control" type="text" name="nome-produto" id="nome-produto-frigobar" value="<?php echo $nomeItem ?>" disabled required>
+                        </div>
+
+                        <div class="col-md-4">
+                            <label class="font-1-s" for="preco-unit">Valor unit.</label>
+                            <input class="form-control monetario" type="text"  name="preco-unit" id="preco-unit" value="<?php echo $precoUnit ?>" disabled required>
+                        </div>
+                    </div>
+
+                    <div class="mb-3 container-info-produto-frigobar">
+                        <div>
+                            <p class="font-1-xs">Qnt. disponível </p>
+                            <span class="total-item-estoque font-1-m-b"><?php echo $totalItensDiponivel ?></span>
+                        </div>
+                    </div>
+                
+                    <div class="mb-3">
+                        <label class="font-1-s" for="quantidade">Quantidade</label>
+                        <input class="form-control quantidade" type="number" min="1" max="" name="quantidade" id="quantidade" required>
+                        <div class="invalid-feedback"></div>
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="font-1-s" for="valor-total">Valor total</label>
+                        <input class="form-control monetario" type="text" name="valor-total" id="valor-total" disabled required>
+                    </div>
+
+
+                    <?php if(!empty($mensagem)){ ?>  
+                        <div class="alert alert-success alert-dismissible fade show" role="alert">
+                            <?php echo $mensagem ?>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div> 
+                    <?php }else {
+                            echo '';
+                        }
+                    ?>
+
+                    <div class="modal-footer form-container-button">
+                        <button type="button" class="btn btn-secondary btn-modal-cancelar" data-bs-dismiss="modal">Cancelar</button>
+                        <button class='btn btn-primary btn-adicionar' type="submit">Adicionar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+<script>
+    $('.monetario').mask('000.000.000.000.000,00', {reverse: true});
+
+</script>
