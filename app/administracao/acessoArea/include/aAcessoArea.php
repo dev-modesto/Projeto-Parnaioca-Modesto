90a5232af58b078f5248fd28631a77c7202e436b
dev-modesto <gabriel_26@outlook.com.br>
2024-08-03 21:04:58 -0300

    feat: validação atualizar acesso

--- app/administracao/acessoArea/include/aAcessoArea.php
+++ app/administracao/acessoArea/include/aAcessoArea.php
@@ -1,31 +1,71 @@
 <?php
     include $_SERVER['DOCUMENT_ROOT'] . '/Projeto-Parnaioca-Modesto/config/config.php';
     include ARQUIVO_CONEXAO;
+    include ARQUIVO_FUNCAO_SQL;
+
+    session_start();
+
+    if (session_status() == PHP_SESSION_ACTIVE) {
+        $idLogado = $_SESSION['id'];
+    }
 
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $id = $_POST['id-funcionario'];
+        $idFuncionario = $_POST['id-funcionario'];
 
         $sac = isset($_POST['sac']) ? $_POST['sac'] : 0;
         $logistica = isset($_POST['logistica']) ? $_POST['logistica'] : 0;
         $administracao = isset($_POST['administracao']) ? $_POST['administracao'] : 0;
 
-        $sql = 
-            "UPDATE tbl_acesso_area 
-            SET 
-                sac = $sac, 
-                logistica = $logistica, 
-                administracao = $administracao 
-            WHERE id_funcionario = $id
-        ";
+        $nivelAcessoLogado = verificaNivelAcesso($con, $idLogado);
+        $nivelLogado = $nivelAcessoLogado['nivel_acesso'];
+        $idFuncionarioNivelLogado = $nivelAcessoLogado['id_funcionario'];
 
-        if(mysqli_query($con, $sql)) {
+        $nivelAcessoFuncionarioAtualizar = verificaNivelAcesso($con, $idFuncionario);
+        $nivelFuncionarioAtualizar = $nivelAcessoFuncionarioAtualizar['nivel_acesso'];
+
+        if ($nivelFuncionarioAtualizar > 0 && $nivelLogado == 0) {
+            $mensagem = "Você não possui autorização para atualizar as informações deste funcionário.";
+            header('location: ../index.php?msgInvalida=' . $mensagem);
+            die();
+        } 
+
+        mysqli_begin_transaction($con);
+
+        try {
+
+            $sql = 
+                "UPDATE tbl_acesso_area 
+                SET 
+                    sac = $sac, 
+                    logistica = $logistica, 
+                    administracao = $administracao 
+                WHERE id_funcionario = $idLogado
+            ";
+
+            mysqli_query($con, $sql);
+                            
+            // log operações
+                $nomeTabela = 'tbl_acesso_area';
+                $idRegistro = $idFuncionario;
+                $tpOperacao = 'atualizacao';
+                $descricao = 'Funcionário atualizado ID: ' . $idFuncionario;
+                logOperacao($con,$idLogado,$nomeTabela,$idRegistro,$tpOperacao,$descricao);
+            // 
+
+            mysqli_commit($con);
             header('location: ../index.php?msg=Atualizado com sucesso!');
-        
-        } else {
-            echo "Erro ao gravar: " . mysqli_error($con);
+
+        } catch (Exception $e) {
+            mysqli_rollback($con);
+            $mensagem = "Ocorreu um erro. Não foi possível realizar a operação.";
+            header('location: ../index.php?msgInvalida=' . $mensagem);
+
+        } finally {
+            mysqli_close($con);
         }
         
-        mysqli_close($con);
+    } else {
+        $mensagem = "";
     }
 
 ?>
