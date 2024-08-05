d3430cb01513ceb89b445454c6bb88f8507ddb73
Gabriel.Modesto <gabriel_26@outlook.com.br>
2024-08-05 11:05:39 -0300

    feat: adicionado json tabela pt-br

--- js/table.js
+++ js/table.js
@@ -3,6 +3,10 @@ new DataTable('#myTable', {
     language: {
         url: '//cdn.datatables.net/plug-ins/2.0.8/i18n/pt-BR.json',
     },
+    
+    // language: {
+    //     url: '../config/pt_br.json'
+    // },
 });
 
 $(document).ready( function () {
